8d0523985df212223695156234a68f55013d5e85
Emmanuel Flores <emmanuel.flores.f.o@gmail.com>
2026-09-16 23:51:40 -0600

    Commit 2: trabajando con variables

--- variables.py
+++ variables.py
@@ -1,4 +1,9 @@
 # Esta sesión será para aprender el uso de variables
-print("hola mundo")
-print("hoy es miercoles")
-print("mañana es jueves")
+print("Sistema de mensajería")
+distancia_km = 40.7
+peso_kg = 2.0
+es_urgente = True
+zona = "B" 
+
+distancia_km = distancia_km + 16
+print(distancia_km)   
